eff716858486442939d83a4c7fda30ec7fc55be3
Jeonjunhowi <jjh63360@kau.kr>
2022-07-08 15:34:41 +0900

    fix: 더이상 병원 정보 로그를 남기지 않음.

--- src/app/Location/locationController.js
+++ src/app/Location/locationController.js
@@ -7,8 +7,5 @@ exports.getHospitalLocationByLatitLongit = async function (req, res) {
     const getHospitalLocationResponse = await locationProvider.getHospitalLocationByLatitLongit(
         req.params
     )
-
-    console.log(getHospitalLocationResponse)
-
     return res.send(getHospitalLocationResponse);
 }
