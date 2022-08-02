460841ef2752851178d588c1ef329c3649f8b29a
Jeonjunhowi <jjh63360@kau.kr>
2022-08-02 13:05:51 +0900

    fix: route indexing bug

--- src/app/Review/reviewRoute.js
+++ src/app/Review/reviewRoute.js
@@ -2,5 +2,5 @@ module.exports = function(app){
     const review = require('./reviewController');
 
     //1. 리뷰 남기기
-    app.post('app/hospital/review', review.postReview);
+    //app.post('app/hospital/review', review.postReview);
 }
