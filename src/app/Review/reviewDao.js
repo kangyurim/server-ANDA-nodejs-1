ed8a201c077d8ea9e5ea783f76100b98cb9cd6bb
Jeonjunhowi <jjh63360@kau.kr>
2022-08-16 13:29:16 +0900

    add: review 작성 -> 사진이 있는 리뷰 작성 가능

--- src/app/Review/reviewDao.js
+++ src/app/Review/reviewDao.js
@@ -1,23 +1,49 @@
-async function insertReview(connection, insertReviewParams) {
+async function insertReview(connect, insertReviewParams) {
+    let result = new Object();
+    console.log(insertReviewParams)
     const insertReviewQuery = `
         insert into Review(ophthalmologyId, userId, score, reviewText)
-        VALUES (?, ?, ?, ?);
+        VALUES (${insertReviewParams.hospitalId}, ${insertReviewParams.writerId}, ${insertReviewParams.score}, '${insertReviewParams.content}');
     `;
+    console.log("good 6");
+    const insertReviewRow = await connect.query(insertReviewQuery);
 
-    const insertReviewRow = await connection.query(
-        insertReviewQuery,
-        insertReviewParams
-    );
+    if(insertReviewRow[0].affectedRows == 1)
+    {
+        result.titleInptRes = 'SUCCESS';
+        const insertId = insertReviewRow[0].insertId;
+       
+        if(insertReviewParams.pictureUrls.length != 0)
+        {
+            console.log(insertId);
+            for(var i in insertReviewParams.pictureUrls)
+            {   const insertMediaQuery = `INSERT INTO ReviewMedia(reviewId, picURL) VALUES(${insertId}, ?)`
+                const insertMediaQueryRes = await connect.query(insertMediaQuery, insertReviewParams.pictureUrls[i])
+                if(insertMediaQueryRes[0].affectedRows != 1) 
+                {
+                    result.mediaInptRes = 'FAIL';
+                    break;
+                }
+            }
+            result.mediaInptRes = 'SUCCESS';
+        }
+        else if(insertReviewParams.pictureUrls.length == 0)
+        {
+            result.mediaInptRes = 'NULL BUT SUCCESS';
+        }
+    }
+    else  result.titleInptRes = 'FAIL';
 
-    return insertReviewRow;
-};
-async function insertReviewImg(connection, insertReviewImgParams) {
+    return result
+}
+
+async function insertReviewImg(connect, insertReviewImgParams) {
     const insertReviewImgQuery = `
         INSERT INTO ReviewImgUrl(ReviewId, imgUrl)
         VALUES (?, ?);
     `;
 
-    const insertReviewImgRow = await connection.query(insertReviewImgQuery, insertReviewImgParams);
+    const insertReviewImgRow = await connect.query(insertReviewImgQuery, insertReviewImgParams);
 
     return insertReviewImgRow;
 }
